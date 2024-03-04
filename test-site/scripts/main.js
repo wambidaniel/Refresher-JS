c15ec7e4d8759a01564d889aae38a81716bb57ef
danielwambi <wambidanielcollins256@gmail.com>
2024-03-04 09:03:52 +0300

    first commit

---
+++ test-site/scripts/main.js
@@ -0,0 +1,30 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/fire-image.jpg");
+  } else {
+    myImage.setAttribute("src", "images/x.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+
